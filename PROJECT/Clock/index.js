86cb2b9afea638cf28d7e7b31bf4424c5c370075
Narpat Aanjana <narpatanjana0@gmail.com>
2023-04-08 23:42:16 +0530

    Add files via upload

---
+++ PROJECT/Clock/index.js
@@ -0,0 +1,26 @@
+const hourE1 = document.querySelector(".hour")
+const minuteE1 = document.querySelector(".minute")
+const secondE1 = document.querySelector(".second")
+
+function clock() {
+
+    const currentDate = new Date();
+    setTimeout(clock, 1000);
+
+    const hour = currentDate.getHours();
+    const minute = currentDate.getMinutes();
+    const second = currentDate.getSeconds();
+
+    const hourDeg = (hour / 12)*360;
+    hourE1.style.transform = `rotate(${hourDeg}deg)`;
+
+    const minuteDeg = (minute / 60)*360;
+    minuteE1.style.transform = `rotate(${minuteDeg}deg)`;
+
+    const secondDeg = (second / 60)*360;
+    secondE1.style.transform = `rotate(${secondDeg}deg)`;
+
+
+}
+
+clock()
